6016199d0f4be900404515aa2295411c71132e47
Tianhao Li <2091531393@qq.com>
2023-09-07 17:54:43 +0800

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting htjhjgy6") + ", " + name.toUpperCase（）;
   }
 
   public static void main(String[] args) {
